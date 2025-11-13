d43dde47fcd08727cf51afd1acc9fb89b57957d8
Karthigeyankiruba <karthigeyan.k@unosecur.com>
2025-11-13 13:07:55 +0530

    Add useDebounce hook for improved search performance in DashboardAside component

--- src/layout/DashboardAside/DashboardAside.tsx
+++ src/layout/DashboardAside/DashboardAside.tsx
@@ -1,10 +1,12 @@
 import { EmployeeList } from "../../components/EmployeeList";
 import { Filters } from "../../components/Filters";
 import { useFilters } from "../../contexts/FilterContext";
+import { useDebounce } from "../../hooks/useDebounce";
 import "./DashboardAside.style.css";
 
 const DashboardAside = () => {
   const { search, selectedTeam, setSearch, setSelectedTeam } = useFilters();
+  const debouncedSearch = useDebounce(search, 300);
 
   return (
     <aside className="dashboard-aside">
@@ -15,7 +17,7 @@ const DashboardAside = () => {
           onSearchChange={setSearch}
           onTeamChange={setSelectedTeam}
         />
-        <EmployeeList search={search} team={selectedTeam} />
+        <EmployeeList search={debouncedSearch} team={selectedTeam} />
       </div>
     </aside>
   );
